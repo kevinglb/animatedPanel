06ebbcd4ad9af847c3889deae7d2f581227898ae
unknown <Kevin.Gu@CNSHAPWW3051.nurun.com>
2016-03-02 13:44:49 +0800

    update js and css for changes

--- js/animated.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	$("input.form-control").focus(function(){
-		var d = $(this).parent().find(".form-group");
-		d.addclass("focused");
-	});	
-}())
--- js/animatedPanel.js
+++ js/animatedPanel.js
@@ -1,6 +1,12 @@
 (function(){
-	$("input.form-control").focus(function(){
-		var d = $(this).parent().find(".form-group");
-		d.addClass("focused");
+	$("input.form-control").on("focus blur",function(e){
+		var d = $(this).parent(".form-group");
+		d.toggleClass("focused", (e.type === "focus" || this.value.length > 0));
 	});	
+
+	$(".switchBtn").on("click",function(){
+		$(".panelOuter").toggleClass("triggered");
+		$(".panelInner").toggleClass("triggered");
+	});
+
 }())
