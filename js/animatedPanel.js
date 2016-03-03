9539d4486e68efc55906d41f829250ff0a2cbedf
unknown <Kevin.Gu@CNSHAPWW3051.nurun.com>
2016-03-03 10:34:49 +0800

    1. set css for showPassBtn 2. show/hied password by change the type of the input using pure js (not a scure way by cannot find other methods) 3. using for() instead of foreach() as the latter doesn't work for btnList when its length is 1

--- js/animatedPanel.js
+++ js/animatedPanel.js
@@ -2,6 +2,7 @@
 	$("input.form-control").on("focus blur",function(e){
 		var d = $(this).parent(".form-group");
 		d.toggleClass("focused", (e.type === "focus" || this.value.length > 0));
+
 	});	
 
 	$(".switchBtn").on("click",function(){
@@ -9,4 +10,25 @@
 		$(".panelInner").toggleClass("triggered");
 	});
 
-}())
+	//toggle show/hide password btn function
+	var btnList = document.getElementsByClassName("showPassBtn");
+	var showPassBtn = btnList.length > 1 ? Array.prototype.slice.call(document.getElementsByClassName("showPassBtn")):btnList;
+	
+	for(var i=0,len=btnList.length;i<len;i++){
+		var b = btnList[i];
+		b.addEventListener("click",function(event){
+			var btn = event.currentTarget;
+			console.log(btn);
+			var input = event.target.previousElementSibling;
+			if(input.classList.contains("form-control")){
+				if(input.getAttribute("type") === "password"){
+					input.setAttribute("type","text");
+					btn.innerHTML("Hide");
+				}else{
+					input.setAttribute("type","password");
+					btn.innerHTML ("Show");
+				}
+			}
+		});
+	}
+}());
